09aeb2bca788131a760e6570ca5efbf218e31558
Lucas <lucas.nascimento@logicsp.com.br>
2019-02-07 14:05:07 -0200

    classe main

---
+++ ed/src/ed/VetorTeste.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed;
+
+/**
+ *
+ * @author lucas
+ */
+public class VetorTeste {
+    public static void main(String[] args) {
+        
+        Aluno a1 = new Aluno("joão");
+        Aluno a2 = new Aluno("josé");
+    }
+}
