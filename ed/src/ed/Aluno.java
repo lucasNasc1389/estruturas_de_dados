f69767064f0cf2cf7cbf5bc8b7795c1aba560af8
Lucas <lucas.nascimento@logicsp.com.br>
2019-02-07 13:43:11 -0200

    first

---
+++ ed/src/ed/Aluno.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author lucas
+ */
+public class Aluno {
+    private String nome;
+
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        Aluno outro = (Aluno) obj;
+        return outro.getNome().equals(this.nome);
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    
+    
+    
+}
---
+++ ed/src/ed/Vetor.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author lucas
+ */
+public class Vetor {
+    
+    private Aluno[] alunos = new Aluno[100];
+    
+    public void adiciona(Aluno aluno){
+        
+    }
+    
+    public Aluno pega(int posicao) {
+        return null;
+        
+    }
+    
+    public void remove(int posicao) {
+        
+    }
+    
+    public boolean contem(Aluno aluno) {
+        return false;
+        
+    }
+    
+    public int tamanho() {
+        return 0;
+    }
+    
+    public String toString() {
+        return Arrays.toString(alunos);
+    }
+    
+}
